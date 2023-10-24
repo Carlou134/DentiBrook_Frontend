6aac017fd557fb593ab936b85c563c38df82d7ab
Victor Alejandro Arévalo Díaz <u202112543@upc.edu.pe>
2023-10-23 23:48:08 -0500

    servicio
    servicio

---
+++ src/app/component/servicio/creaedita-servicio/creaedita-servicio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creaedita-servicio',
+  templateUrl: './creaedita-servicio.component.html',
+  styleUrls: ['./creaedita-servicio.component.css']
+})
+export class CreaeditaServicioComponent {
+
+}
