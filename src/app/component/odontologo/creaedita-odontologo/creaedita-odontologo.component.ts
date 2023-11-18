8bbe80de5882b574ecf0de3d62b962a46af6d281
Victor Alejandro Arévalo Díaz <u202112543@upc.edu.pe>
2023-11-18 03:25:10 -0500

    pls

--- src/app/component/odontologo/creaedita-odontologo/creaedita-odontologo.component.ts
+++ src/app/component/odontologo/creaedita-odontologo/creaedita-odontologo.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Odontologo } from 'src/app/model/odontologo';
 
 @Component({
   selector: 'app-creaedita-odontologo',
   templateUrl: './creaedita-odontologo.component.html',
   styleUrls: ['./creaedita-odontologo.component.css']
 })
-export class CreaeditaOdontologoComponent {
+export class CreaeditaOdontologoComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  odontologo: Odontologo=new Odontologo();
+  mensaje: string = '';
 
+  ngOnInit(): void {
+  
+}
 }
