f808bea01835627ef46f224879ceed4c0ed7034b
Victor Alejandro Arévalo Díaz <u202112543@upc.edu.pe>
2023-11-18 03:34:38 -0500

    sí

--- src/app/component/odontologo/creaedita-odontologo/creaedita-odontologo.component.ts
+++ src/app/component/odontologo/creaedita-odontologo/creaedita-odontologo.component.ts
@@ -11,7 +11,7 @@ export class CreaeditaOdontologoComponent implements OnInit {
   form: FormGroup = new FormGroup({});
   odontologo: Odontologo=new Odontologo();
   mensaje: string = '';
-
+  
   ngOnInit(): void {
   
 }
